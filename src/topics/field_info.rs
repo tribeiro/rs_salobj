eba7549ae5a3cfc1bfb6b71fa5b3140fe37053a8
Tiago Ribeiro <tribeiro@lsst.org>
2022-07-20 07:57:13 -0700

    In `topics/field_info` module:
    * Expand `SalType` to include all types supported by SAL. Note that in the Python code the type support is much simpler, all integers subtypes are simply treated as `int`, even the unsigned variants. We may want to simplify what we support.
    * Add implementation for `FieldInfo` traits:
    	* Add private `get_default_scalar_value` trait that returns the default value for a specific sal type.
    	* Add public `new` trait to create `FieldInfo` from a set of parameters. Note that `default_scalar_value` is not part of the parameters, as it is inferred from the inputs.

--- src/topics/field_info.rs
+++ src/topics/field_info.rs
@@ -1,10 +1,18 @@
 extern crate serde;
 
 #[derive(Deserialize, Debug)]
-enum SalType {
+pub enum SalType {
     SalBool(bool),
+    SalByte(u8),
+    SalShort(i16),
+    SalInt(isize),
+    SalLong(i32),
+    SalLongLong(i64),
+    SalUnsignedShort(u16),
+    SalUnsignedInt(usize),
+    SalUnsignedLong(u32),
     SalFloat(f32),
-    SalInt(u32),
+    SalDouble(f64),
     SalString(String),
 }
 
@@ -24,4 +32,44 @@ pub struct FieldInfo {
     /// For a scalar: the default value.
     /// For an array: the default value of one element.
     default_scalar_value: SalType,
+}
+
+impl FieldInfo {
+
+    /// Create new FieldInfo.
+    pub fn new(
+        name: &str,
+        sal_type: &str,
+        count: usize,
+        units: &str,
+        description: &str,
+    ) -> FieldInfo {
+        FieldInfo {
+            name: String::from(name),
+            sal_type: String::from(sal_type),
+            count: count,
+            units: String::from(units),
+            description: String::from(description),
+            default_scalar_value: FieldInfo::get_default_scalar_value(sal_type),
+        }
+    }
+
+    /// Return the default value for the specified sal type.
+    fn get_default_scalar_value(sal_type: &str) -> SalType {
+        match sal_type {
+            "boolean" => SalType::SalBool(false),
+            "byte" => SalType::SalByte(0),
+            "short" => SalType::SalShort(0),
+            "int" => SalType::SalInt(0),
+            "long" => SalType::SalLong(0),
+            "long long" => SalType::SalLongLong(0),
+            "unsigned short" => SalType::SalUnsignedShort(0),
+            "unsigned int" => SalType::SalUnsignedInt(0),
+            "unsigned long" => SalType::SalUnsignedLong(0),
+            "float" => SalType::SalFloat(0.0),
+            "double" => SalType::SalDouble(0.0),
+            "string" => SalType::SalString(String::from("")),
+            _ => panic!("Unrecognized SAL type '{sal_type}'"),
+        }
+    }
 }
