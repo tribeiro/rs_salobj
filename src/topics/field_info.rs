8baa7e3e76b6b7ee47d40e281bf960e52098294c
Tiago Ribeiro <tribeiro@lsst.org>
2022-07-22 13:31:39 -0700

    Implement support for generating avro schema for topic fields.
    In topics/field_info submodule:
    
    * Add new `AvroSchemaField` struct with derived Serialize and Deserialize macros.
      This struct represents a topic field in avro schema.
    * Add `get_avro_type_from_sal_type` and `make_avro_schema` methods to `FieldInfo` traits.
    
      * `FieldInfo::get_avro_type_from_sal_type` receives a str reference, which must be a valid sal type, and return the equivalent avro type.
      * `FieldInfo.make_avro_schema` returns an instance of `AvroSchemaField` for the topic field.

--- src/topics/field_info.rs
+++ src/topics/field_info.rs
@@ -18,6 +18,17 @@ pub enum SalType {
     SalString(String),
 }
 
+/// Avro Schema field.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AvroSchemaField {
+    name: String,
+    #[serde(rename = "type")]
+    avro_type: String,
+    default: String,
+    description: String,
+    units: String,
+}
+
 /// Information about one field of a topic.
 #[derive(Deserialize, Debug)]
 pub struct FieldInfo {
@@ -75,6 +86,16 @@ impl FieldInfo {
         }
     }
 
+    pub fn make_avro_schema(&self) -> AvroSchemaField {
+        AvroSchemaField {
+            name: self.name.to_owned(),
+            avro_type: FieldInfo::get_avro_type_from_sal_type(&self.sal_type).to_owned(),
+            default: self.default_scalar_value.to_string(),
+            description: self.description.to_owned(),
+            units: self.units.to_owned(),
+        }
+    }
+
     /// Return the default value for the specified sal type.
     fn get_default_scalar_value(sal_type: &str) -> SalType {
         match sal_type {
@@ -92,5 +113,43 @@ impl FieldInfo {
             "string" => SalType::SalString(String::from("")),
             _ => panic!("Unrecognized SAL type '{sal_type}'"),
         }
+    }
+
+    /// Return the avro type name from the sal type name.
+    fn get_avro_type_from_sal_type(sal_type: &str) -> &str {
+        match sal_type {
+            "boolean" => "boolean",
+            "byte" => "int",
+            "short" => "int",
+            "int" => "int",
+            "long" => "long",
+            "long long" => "long",
+            "unsigned short" => "int",
+            "unsigned int" => "int",
+            "unsigned long" => "long",
+            "float" => "float",
+            "double" => "double",
+            "string" => "string",
+            _ => panic!("Unrecognized SAL type '{sal_type}'"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn make_avro_schema() {
+        let field_info = FieldInfo::new("value", "string", 1, "unitless", "Test value.");
+
+        let avro_schema = field_info.make_avro_schema();
+        let avro_schema_str = serde_json::to_string(&avro_schema).unwrap();
+
+        assert_eq!(
+            avro_schema_str,
+            r#"{"name":"value","type":"string","default":"","description":"Test value.","units":"unitless"}"#
+        )
     }
 }
