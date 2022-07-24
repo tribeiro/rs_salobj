e18823e4c1bbae8f3e4c4e6dd031a70b8ea82888
Tiago Ribeiro <tribeiro@lsst.org>
2022-07-24 13:30:16 -0700

    Initial refactor of `SalInfo` struct, towards more complete implementation. At this stage, implement the following changes:
    * Hold an owned copy of `name` and `index`.
    * Remove instances of `command_names`, `event_names` and `telemetry_names` from `SalInfo`.
    * Add `component_info` to the data structure.
    * Update `SalInfo::new`, to create an instance of `ComponentInfo`.
      It reads the environment variable `LSST_TOPIC_SUBNAME` to set the `topic_subname` of `ComponentInfo`.
      It also checks that if index is different than zero, the component must be indexed, and will panic otherwise.
    * Add methods to `SalInfo` to relay whether the component is indexed or not from `ComponentInfo`.
    * Add method to return a string with the component name and index, suppressing the index for non-indexed components e.g. Test:1 or ATMCS.
    * Refactor methods to return topic names (commands, events and telemetry) to return values provided by `ComponetInfo`.
    * Add unit tests for new methods.

--- src/sal_info.rs
+++ src/sal_info.rs
@@ -1,33 +1,123 @@
+use crate::component_info::ComponentInfo;
 use crate::domain;
+use std::env;
 
 ///Information for one SAL component and index.
 pub struct SalInfo<'a> {
     domain: &'a mut domain::Domain,
-    name: &'a str,
-    index: &'a usize,
-    command_names: Vec<String>,
-    event_names: Vec<String>,
-    telemetry_names: Vec<String>,
+    name: String,
+    index: usize,
+    component_info: ComponentInfo,
 }
 
 impl<'a> SalInfo<'a> {
     pub fn new(domain: &'a mut domain::Domain, name: &'a str, index: &'a usize) -> SalInfo<'a> {
         SalInfo {
+        let topic_subname = match env::var("LSST_TOPIC_SUBNAME") {
+            Ok(val) => val,
+            Err(_) => panic!("You must define environment variable LSST_TOPIC_SUBNAME"),
+        };
+        let component_info = ComponentInfo::new(name, &topic_subname);
+
+        if index != 0 && !component_info.is_indexed() {
+            panic!("Invalid index={index}. Component {name} is not indexed. Index must be 0.")
+        }
+
             domain: domain,
             name: name,
+            name: name.to_owned(),
             index: index,
             command_names: Vec::new(),
             event_names: Vec::new(),
             telemetry_names: Vec::new(),
+            component_info: component_info,
+    /// Is the component indexed?
+    pub fn is_indexed(&self) -> bool {
+        self.component_info.is_indexed()
+    }
+
+    /// Get name[:index]
+    ///
+    /// The suffix is only passed if the component is index.
+    pub fn get_name_index(&self) -> String {
+        if self.is_indexed() {
+            format!("{}:{}", self.name, self.index)
+        } else {
+            format!("{}", self.name)
         }
     }
-    pub fn get_command_names(&self) -> &Vec<String> {
-        &self.command_names
+
+    /// Get name of all commands topics.
+    pub fn get_command_names(&self) -> Vec<String> {
+        self.component_info.get_command_names()
     }
-    pub fn get_event_names(&self) -> &Vec<String> {
-        &self.event_names
+
+    /// Get names of all events topics.
+    pub fn get_event_names(&self) -> Vec<String> {
+        self.component_info.get_event_names()
     }
-    pub fn get_telemetry_names(&self) -> &Vec<String> {
-        &self.telemetry_names
+
+    /// Get names of all telemetry topics.
+    pub fn get_telemetry_names(&self) -> Vec<String> {
+        self.component_info.get_telemetry_names()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn sal_info_get_command_names() {
+        let domain = Rc::new(RefCell::new(domain::Domain::new()));
+        let sal_info = SalInfo::new(domain, "Test", 1);
+
+        let command_names = sal_info.get_command_names();
+
+        assert!(command_names.contains(&"Test_command_setScalars".to_owned()))
+    }
+
+    #[test]
+    fn sal_info_get_event_names() {
+        let domain = Rc::new(RefCell::new(domain::Domain::new()));
+        let sal_info = SalInfo::new(domain, "Test", 1);
+
+        let event_names = sal_info.get_event_names();
+
+        assert!(event_names.contains(&"Test_logevent_scalars".to_owned()))
+    }
+
+    #[test]
+    fn sal_info_get_telemetry_names() {
+        let domain = Rc::new(RefCell::new(domain::Domain::new()));
+        let sal_info = SalInfo::new(domain, "Test", 1);
+
+        let telemetry_names = sal_info.get_telemetry_names();
+
+        assert!(telemetry_names.contains(&"Test_scalars".to_owned()))
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid index=1. Component ATMCS is not indexed. Index must be 0.")]
+    fn panic_if_index_for_non_indexed() {
+        let domain = Rc::new(RefCell::new(domain::Domain::new()));
+        let _ = SalInfo::new(domain, "ATMCS", 1);
+    }
+
+    #[test]
+    fn get_name_index_indexed() {
+        let domain = Rc::new(RefCell::new(domain::Domain::new()));
+        let sal_info = SalInfo::new(domain, "Test", 1);
+
+        assert_eq!(sal_info.get_name_index(), "Test:1")
+    }
+
+    #[test]
+    fn get_name_index_non_indexed() {
+        let domain = Rc::new(RefCell::new(domain::Domain::new()));
+        let sal_info = SalInfo::new(domain, "ATMCS", 0);
+
+        assert_eq!(sal_info.get_name_index(), "ATMCS")
     }
 }
