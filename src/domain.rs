6ae3311e51587bc5836c7b48dc08882dacb8d184
Tiago Ribeiro <tribeiro@lsst.org>
2022-07-24 13:47:16 -0700

    Minimum implementation of the `Domain` struct. * Add a `sal_info_set` attribute that contains a `RefCell` with a vector of weak references to `SalInfo`. * Add a constructor method `new` to create `Remote`. * Add `get_default_identity` method. Currently it only returns a fixed string with `user@localhost`. Future implementations should get this information from the system. * Add methods to add and remove `SalInfo` weak references to `sal_info_set`.

--- src/domain.rs
+++ src/domain.rs
@@ -1,1 +1,51 @@
-pub struct Domain {}
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+use crate::sal_info::SalInfo;
+
+pub struct Domain {
+    sal_info_set: RefCell<Vec<Weak<SalInfo>>>,
+}
+
+impl Domain {
+    /// Create a new instance of Domain,
+    pub fn new() -> Domain {
+        Domain {
+            sal_info_set: RefCell::new(Vec::new()),
+        }
+    }
+
+    /// Return the default identify.
+    pub fn get_default_identity(&self) -> String {
+        "user@localhost".to_owned()
+    }
+
+    /// Add the specified salinfo to the internal registry.
+    ///
+    /// The input pointer is downgraded to a week reference and added to a
+    /// vector list.
+    pub fn add_salinfo(&self, sal_info: &Rc<SalInfo>) {
+        self.sal_info_set.borrow_mut().push(Rc::downgrade(sal_info));
+    }
+
+    /// Remove the specified salinfo to the internal registry.
+    ///
+    /// Objects are removed using `swap_remove`, which is O(1) but does not
+    /// preserve ordering.
+    pub fn remove_salinfo(&self, sal_info: &Rc<SalInfo>) -> bool {
+        match self
+            .sal_info_set
+            .borrow()
+            .iter()
+            .position(|x| x.ptr_eq(&Rc::downgrade(sal_info)))
+        {
+            Some(index) => {
+                self.sal_info_set.borrow_mut().swap_remove(index);
+                true
+            }
+            None => false,
+        }
+    }
+}
