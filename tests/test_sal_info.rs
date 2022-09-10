1db29e1556bc7db6a29b2e10506ad81f04e2c5bc
Tiago Ribeiro <tribeiro@lsst.org>
2022-09-10 14:43:38 -0700

    In `tests/test_sal_info.rs`, add test for `add_writer` method.

--- tests/test_sal_info.rs
+++ tests/test_sal_info.rs
@@ -1,4 +1,8 @@
-use salobj::{domain, sal_info::SalInfo, topics::read_topic::ReadTopic};
+use salobj::{
+    domain,
+    sal_info::SalInfo,
+    topics::{read_topic::ReadTopic, write_topic::WriteTopic},
+};
 use std::{cell::RefCell, rc::Rc};
 
 #[test]
@@ -11,4 +15,16 @@ fn test_add_reader() {
     sal_info.add_reader(read_topic);
 
     assert!(sal_info.has_read_topic("Test_scalars"));
+}
+
+#[test]
+fn test_add_writer() {
+    let domain = Rc::new(RefCell::new(domain::Domain::new()));
+    let sal_info = SalInfo::new(domain, "Test", 1);
+
+    let write_topic = WriteTopic::new(Rc::clone(&sal_info), "Test_scalars");
+
+    sal_info.add_writer(write_topic);
+
+    assert!(sal_info.has_write_topic("Test_scalars"));
 }
