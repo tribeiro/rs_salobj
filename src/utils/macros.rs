db01d6c3319bb96153c644464567c5b47402ee56
Tiago Ribeiro <tribeiro@lsst.org>
2022-11-26 19:51:46 -0700

    In ``src/utils/macros.rs``, add new lifetime required by ``BaseTopic`` ti tge ``base_topic`` macro.

--- src/utils/macros.rs
+++ src/utils/macros.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! base_topic {
     ($t:ident) => {
-        impl<'a> BaseTopic for $t<'a> {
+        impl<'a, 'b> BaseTopic for $t<'a, 'b> {
             fn get_topic_info(&self) -> &TopicInfo {
                 self.sal_info.get_topic_info(&self.topic_name).unwrap()
             }
