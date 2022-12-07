dbb78206dec730bfa4c1f1e9771cb80ce7ff4bef
Tiago Ribeiro <tribeiro@lsst.org>
2022-12-07 09:06:09 -0300

    Remove ``src/topics/remote_event.rs`` and ``src/topics/remote_telemetry.rs``, as these are unnecessary.

--- src/topics/mod.rs
+++ src/topics/mod.rs
@@ -2,8 +2,6 @@ pub mod base_topic;
 pub mod field_info;
 pub mod read_topic;
 pub mod remote_command;
-pub mod remote_event;
-pub mod remote_telemetry;
 pub mod sal_objects;
 pub mod topic_info;
 pub mod write_topic;
