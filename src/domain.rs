c924266cfa58fe7e36fb2ad182de6c405f3434bc
Tiago Ribeiro <tribeiro@lsst.org>
2023-02-03 11:36:30 -0700

    In ``domain.rs``, updates to ``Domain`` to support registering kafka topics:
    * Add an instance of ``KafkaClient``.
    * Add new method ``register_topics``, which basically calls ``KafkaClient.register_topics`` but handles the process of registering topics and reassuring that the topics are available later.
      The method has a pooling loop that requests the available topics in the Kafka cluster and stops when all the topics are registered.
      It will try this a ``MAX_ITER_LOAD_METADATA`` times, waiting ``POOL_CLIENT_WAIT_TIME`` at every iteration.
      If it fails to register the topics it returns an ``Error``.
    * Add new method ``get_client_hosts`` that returns a list of client host addresses.
      For now this simply return a static value but it will be updated in the future to handle more general use cases.

--- src/domain.rs
+++ src/domain.rs
@@ -1,9 +1,15 @@
-use std::process;
+use kafka::client::KafkaClient;
+use kafka::error::Error as KafkaError;
+use std::{process, thread, time::Duration};
 use whoami;
+
+const MAX_ITER_LOAD_METADATA: u8 = 5;
+const POOL_CLIENT_WAIT_TIME: Duration = Duration::from_millis(500);
 
 pub struct Domain {
     origin: u32,
     identity: Option<String>,
+    kafka_client: KafkaClient,
 }
 
 impl Domain {
@@ -12,6 +18,7 @@ impl Domain {
         Domain {
             origin: process::id(),
             identity: None,
+            kafka_client: KafkaClient::new(Domain::get_client_hosts()),
         }
     }
 
@@ -31,6 +38,46 @@ impl Domain {
             Some(identity) => identity.to_owned(),
             None => self.get_default_identity(),
         }
+    }
+
+    /// Register topics.
+    pub fn register_topics<T: AsRef<str>>(&mut self, topics: &[T]) -> Result<(), KafkaError> {
+        for _ in 0..MAX_ITER_LOAD_METADATA {
+            let result = self.kafka_client.load_metadata(topics);
+            match result {
+                Ok(_) => {
+                    let partitions_unloaded: Vec<bool> = topics
+                        .iter()
+                        .filter_map(|topic| {
+                            if self
+                                .kafka_client
+                                .topics()
+                                .partitions(topic.as_ref().into())
+                                .map(|p| p.len())
+                                .unwrap_or(0)
+                                > 0
+                            {
+                                None
+                            } else {
+                                Some(true)
+                            }
+                        })
+                        .collect();
+                    if partitions_unloaded.is_empty() {
+                        return Ok(());
+                    }
+                }
+                Err(err) => return Err(err),
+            }
+            thread::sleep(POOL_CLIENT_WAIT_TIME);
+        }
+        Ok(())
+    }
+
+    /// Get client host address.
+    pub fn get_client_hosts() -> Vec<String> {
+        // FIXME: Handle general case (tribeiro/rs_salobj#42).
+        vec!["localhost:9092".to_owned()]
     }
 }
 
