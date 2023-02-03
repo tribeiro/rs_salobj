5e35c5dcf29a0705aa4571140718737abd5726e8
Tiago Ribeiro <tribeiro@lsst.org>
2023-02-03 11:24:16 -0700

    In ``topics/topic_info.rs``, replace ``HashMap`` by ``BTreeMap`` in ``TopicInfo.fields`` to make sure the fields are stored orderly.
    This is required to generate stable avro schema.

--- src/topics/topic_info.rs
+++ src/topics/topic_info.rs
@@ -1,13 +1,13 @@
 use crate::topics::field_info;
 use crate::topics::sal_objects::SalTopic;
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 
 /// Information about one topic.
 pub struct TopicInfo {
     component_name: String,
     topic_subname: String,
     topic_name: String,
-    fields: HashMap<String, field_info::FieldInfo>,
+    fields: BTreeMap<String, field_info::FieldInfo>,
     description: String,
     partitions: usize,
 }
@@ -30,7 +30,7 @@ impl TopicInfo {
             component_name: String::new(),
             topic_subname: String::new(),
             topic_name: String::new(),
-            fields: HashMap::new(),
+            fields: BTreeMap::new(),
             description: String::new(),
             partitions: 0,
         }
@@ -41,7 +41,7 @@ impl TopicInfo {
             component_name: String::from(component_name),
             topic_subname: String::from(topic_subname),
             topic_name: String::from("ackcmd"),
-            fields: TopicInfo::get_ackcmd_fields(indexed),
+            fields: TopicInfo::get_ackcmd_fields(indexed).into_iter().collect(),
             description: String::from("Command acknowledgement"),
             partitions: 1,
         }
