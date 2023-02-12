23bc0181383b2404cb3c978b692d3d95589e85c7
Tiago Ribeiro <tribeiro@lsst.org>
2023-02-12 15:53:00 -0700

    In ``lib``, make some minor improvements in the higher level module documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-//! # Rust SAL Object.
+//! # Object-Oriented Service Abstraction Layer (SAL).
 //!
 //! This library provides an implementation of [salobj][1] in rust.
 //!
-//! It provides a framework used to develop control software for the Vera
-//! Rubin Observatory. The implementation here is based of the kafka-based
-//! Python implementation.
+//! SALObj is a framework used to develop control software for the Vera
+//! Rubin Observatory. The implementation here is based in the original Python
+//! kafka implementation of the library.
 //!
 //! This library is still under heavy development and is, at the time of this
 //! writing, a rust learning exercise. Maybe in a distant future it will be
 //! adopted by the project.
 //!
-//! Some interesting notes. In Spanish and Portuguese "sal" means "salt", which
+//! An interesting note, in Spanish and Portuguese "sal" means "salt", which
 //! is known to make metal "rust".
 //!
 //! # Version history
