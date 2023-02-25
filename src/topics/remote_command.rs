c796ed0029df1c80d365e5e0e665b471847cfd7d
Tiago Ribeiro <tribeiro@lsst.org>
2023-02-25 07:41:51 -0700

    In ``src/topics/remote_command.rs``, update ``RemoteCommand`` to stop using unwrap.
    When executing a command, it will now return an ``AckCmdResult``, which is a ``Result`` with a ``CommandAck`` type in both the ``Ok`` and ``Err`` branches.
    This is done because the ``CommandAck`` object encapsulates both the success and error conditions.

--- src/topics/remote_command.rs
+++ src/topics/remote_command.rs
@@ -8,7 +8,7 @@ use crate::{
 use apache_avro::types::{Record, Value};
 use std::{collections::HashMap, time::Duration};
 
-type Result<T> = std::result::Result<T, T>;
+pub type AckCmdResult = std::result::Result<CommandAck, CommandAck>;
 
 pub struct RemoteCommand {
     command_writer: WriteTopic,
@@ -29,65 +29,70 @@ impl RemoteCommand {
         timeout: Duration,
         wait_done: bool,
         sal_info: &SalInfo<'a>,
-    ) -> Result<CommandAck> {
-        let seq_num = self.command_writer.write(parameters, sal_info).await;
-
+    ) -> AckCmdResult {
         let identity = self.command_writer.get_identity();
         let origin = self.command_writer.get_origin();
-
-        loop {
-            if let Some(Value::Record(ack_cmd)) =
-                self.ack_reader.pop_back(false, timeout, sal_info).await
-            {
-                let data_dict: HashMap<String, Value> = ack_cmd
-                    .into_iter()
-                    .map(|(field, value)| (field, value))
-                    .collect();
-                if *data_dict.get("origin").unwrap() == Value::Long(origin)
-                    && *data_dict.get("identity").unwrap() == Value::String(identity.to_owned())
-                    && *data_dict.get("private_seqNum").unwrap() == Value::Long(seq_num)
+        match self.command_writer.write(parameters, sal_info).await {
+            Ok(seq_num) => loop {
+                if let Some(Value::Record(ack_cmd)) =
+                    self.ack_reader.pop_back(false, timeout, sal_info).await
                 {
-                    let ack = sal_enums::get_ackcmd_code(data_dict.get("ack").unwrap()).clone();
-                    let error: isize = match data_dict.get("error").unwrap() {
-                        Value::Int(error) => *error as isize,
-                        _ => 0,
-                    };
-                    let result = match data_dict.get("result").unwrap() {
-                        Value::String(result) => result.to_owned(),
-                        _ => "".to_string(),
-                    };
+                    let data_dict: HashMap<String, Value> = ack_cmd
+                        .into_iter()
+                        .map(|(field, value)| (field, value))
+                        .collect();
 
-                    let command_ack = CommandAck::new(
-                        ack,
-                        error,
-                        result,
-                        identity.clone(),
+                    if *data_dict.get("origin").unwrap_or(&Value::Long(0)) == Value::Long(origin)
+                        && *data_dict
+                            .get("identity")
+                            .unwrap_or(&Value::String("".to_owned()))
+                            == Value::String(identity.to_owned())
+                        && *data_dict.get("private_seqNum").unwrap_or(&Value::Long(0))
+                            == Value::Long(seq_num)
+                    {
+                        let ack = sal_enums::get_ackcmd_code(data_dict.get("ack")).clone();
+                        let error: isize = match data_dict.get("error") {
+                            Some(Value::Int(error)) => *error as isize,
+                            _ => 0,
+                        };
+                        let result = match data_dict.get("result") {
+                            Some(Value::String(result)) => result.to_owned(),
+                            _ => "".to_string(),
+                        };
+
+                        let command_ack = CommandAck::new(
+                            ack,
+                            error,
+                            result,
+                            identity.clone(),
+                            origin,
+                            timeout,
+                            seq_num,
+                        );
+
+                        if !wait_done {
+                            return Ok(command_ack);
+                        } else if command_ack.is_final() {
+                            if command_ack.is_good() {
+                                return Ok(command_ack);
+                            } else {
+                                return Err(command_ack);
+                            };
+                        }
+                    }
+                } else {
+                    return Err(CommandAck::new(
+                        sal_enums::SalRetCode::CmdNoack,
+                        -1,
+                        "No acknowledgment seen.".to_string(),
+                        identity,
                         origin,
                         timeout,
                         seq_num,
-                    );
-
-                    if !wait_done {
-                        return Ok(command_ack);
-                    } else if command_ack.is_final() {
-                        if command_ack.is_good() {
-                            return Ok(command_ack);
-                        } else {
-                            return Err(command_ack);
-                        };
-                    }
+                    ));
                 }
-            } else {
-                return Err(CommandAck::new(
-                    sal_enums::SalRetCode::CmdNoack,
-                    -1,
-                    "No acknowledgment seen.".to_string(),
-                    identity,
-                    origin,
-                    timeout,
-                    seq_num,
-                ));
-            }
+            },
+            Err(error) => Err(CommandAck::invalid_command(&error.to_string())),
         }
     }
 }
