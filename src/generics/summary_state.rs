d2368a616ff3f0fa20f9dac7e5b91a8aba2663f0
Tiago Ribeiro <tribeiro@lsst.org>
2023-09-20 15:50:38 -0700

    Reformatting in generics/summary_state.

--- src/generics/summary_state.rs
+++ src/generics/summary_state.rs
@@ -1,4 +1,6 @@
-use crate::{sal_enums::State, topics::base_sal_topic::BaseSALTopic, utils::xml_utils::get_default_sal_index};
+use crate::{
+    sal_enums::State, topics::base_sal_topic::BaseSALTopic, utils::xml_utils::get_default_sal_index,
+};
 use base_topic_derive::{add_sal_topic_fields, BaseSALTopic};
 
 #[add_sal_topic_fields]
