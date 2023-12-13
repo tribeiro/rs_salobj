90c71ccd5aa62d844f1353e912a999f447676c5f
Tiago Ribeiro <tribeiro@lsst.org>
2023-12-12 23:39:21 -0700

    In ``src/topics/base_sal_topic.rs``, add method to get the SAL private fields.

--- src/topics/base_sal_topic.rs
+++ src/topics/base_sal_topic.rs
@@ -6,4 +6,13 @@ pub trait BaseSALTopic {
     fn get_private_seq_num(&self) -> i64;
     fn get_private_rcv_stamp(&self) -> f64;
     fn get_sal_index(&self) -> i64;
+    fn set_private_snd_stamp(&mut self, value: f64);
+    fn set_private_efd_stamp(&mut self, value: f64);
+    fn set_private_kafka_stamp(&mut self, value: f64);
+    fn set_private_origin(&mut self, value: i64);
+    fn set_private_identity(&mut self, value: &str);
+    fn set_private_rev_code(&mut self, value: &str);
+    fn set_private_seq_num(&mut self, value: i64);
+    fn set_private_rcv_stamp(&mut self, value: f64);
+    fn set_sal_index(&mut self, value: i64);
 }
