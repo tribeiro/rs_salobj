78ec60c0c3474a99fefbf09ea41bcd49b6915597
Tiago Ribeiro <tribeiro@lsst.org>
2024-01-15 16:43:48 -0700

    In ``src/csc/test_csc/topics/`` update all Test CSC topics definition to have new required imports.

--- src/csc/test_csc/topics/wait.rs
+++ src/csc/test_csc/topics/wait.rs
@@ -1,5 +1,6 @@
 use crate::{topics::base_sal_topic::BaseSALTopic, utils::xml_utils::get_default_sal_index};
 use base_topic_derive::{add_sal_topic_fields, BaseSALTopic};
+use chrono::Utc;
 
 #[add_sal_topic_fields]
 #[derive(Debug, Deserialize, Default, Serialize, BaseSALTopic, Clone)]
